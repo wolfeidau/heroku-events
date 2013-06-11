e2b9e8fbcdb51944a7dfc6caa637a10463066359
Mark Wolfe <mark@wolfe.id.au>
2013-06-12 08:35:06 +1000

    Internal refactor to enable plugable log entry listeners.

--- test/heroku_events.js
+++ test/heroku_events.js
@@ -1,55 +1,87 @@
 var fs = require('fs')
 var split = require('split')
 var fixtures = require('./fixtures.js')
-var LogParser = require('../index.js')
+var heroku_events = require('../index.js')
+var LogParser = heroku_events.LogParser
 var log = require('debug')('test')
 
 var expect = require('chai').expect
 
-describe('Parse a log file', function () {
+describe('LogParser', function () {
 
+  describe('Parse a log file', function () {
 
-  it('should produce a model from sample log file.', function (done) {
-    // model for heroku dynos
-    var model = {}
-    var logParser = new LogParser(model)
-    fs.createReadStream('./test/state.log')
-      .on('end',function () {
-        log('model', model.apps[0].dynos)
-        expect(model.sinkId).equal(fixtures.stateLogModel.sinkId)
-        expect(model.updated).equal(fixtures.stateLogModel.updated)
-        expect(model.dynos).equal(fixtures.stateLogModel.dynos)
-        done()
-      }).pipe(split()).pipe(logParser)
+    it('should produce a model from sample log file.', function (done) {
+      // model for heroku dynos
+      var model = {}
+      var logParser = new LogParser(model)
+      logParser.register(heroku_events.selectors.statChange)
+      fs.createReadStream('./test/state.log')
+        .on('end',function () {
+          log('model', model.apps[0].dynos)
+          expect(model.sinkId).equal(fixtures.stateLogModel.sinkId)
+          expect(model.updated).equal(fixtures.stateLogModel.updated)
+          expect(model.dynos).equal(fixtures.stateLogModel.dynos)
+          done()
+        }).pipe(split()).pipe(logParser)
 
-  })
-
-  it('should fire up events when dynos start', function (done) {
-
-    // model for heroku dynos
-    var model = {}
-    var logParser = new LogParser(model)
-    var starts = [], stops  = [];
-
-    logParser.on('dyno-up', function(app){
-      log('app', 'start', app)
-      starts.push(app)
     })
 
-    logParser.on('dyno-down', function(app){
-      log('app', 'stop', app)
-      stops.push(app)
+    it('should fire up events when dynos start', function (done) {
+
+      // model for heroku dynos
+      var model = {}
+      var logParser = new LogParser(model)
+      logParser.register(heroku_events.selectors.statChange)
+      var starts = [], stops = [];
+
+      logParser.on('dyno-up', function (app) {
+        log('app', 'start', app)
+        starts.push(app)
+      })
+
+      logParser.on('dyno-down', function (app) {
+        log('app', 'stop', app)
+        stops.push(app)
+      })
+
+      fs.createReadStream('./test/state.log')
+        .on('end',function () {
+          expect(starts).to.exist
+          expect(starts.length).to.equal(2)
+          expect(stops.length).to.equal(2)
+          done()
+        }).pipe(split()).pipe(logParser)
+
+
     })
-
-    fs.createReadStream('./test/state.log')
-      .on('end',function () {
-        expect(starts).to.exist
-        expect(starts.length).to.equal(2)
-        expect(stops.length).to.equal(2)
-        done()
-      }).pipe(split()).pipe(logParser)
-
-
   })
 
+  describe('plugins', function () {
+
+
+    it('should enable plugins for raising discovery events.', function () {
+
+      var model = {}
+      var logParser = new LogParser(model)
+
+      var counter = 0
+
+      function logware(logParser) {
+        return function logwareEvent(data) {
+          counter++
+        }
+      }
+
+      logParser.register(logware)
+
+      fs.createReadStream('./test/state.log')
+        .on('end',function () {
+          expect(counter).to.equal(11)
+          done()
+        }).pipe(split()).pipe(logParser)
+
+    })
+
+  })
 })
